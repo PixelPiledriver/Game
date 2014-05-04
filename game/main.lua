ea81d88df13e061b5fea59936f5811421ded7b75
PixelPiledriver <pixelpiledriver@gmail.com>
2014-05-04 14:50:13 -0700

    Setup
    Adding some really basic shit to work with LOVE.
    Nothing really too special here yet.

--- game/main.lua
+++ game/main.lua
@@ -1,3 +1,19 @@
 
 -- dis iz the main file nigga!!
 -- dont be fuckin round wit my mainz!!!!!
+
+
+-- game start
+function love.load()
+	-- load shit
+	-- build shit
+	-- take a shit --> no really --> go take a shit
+end 
+
+
+-- draw call
+-- draw your stuff
+-- make the pretties
+function love.draw()
+	love.graphics.print("monkey", 100, 100)
+end 
