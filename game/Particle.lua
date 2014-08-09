4ee04f1a0026ee84d0d41843e68c2b067f7d5287
PixelPiledriver <pixelpiledriver@gmail.com>
2014-08-08 22:44:22 -0700

    Particle System
    blah,
    doesnt really work yet

--- game/Particle.lua
+++ game/Particle.lua
@@ -3,12 +3,58 @@
 -- KERPOW
 
 
-
-
-local ParticleSystem = {}
-
+local ObjectUpdater = require("ObjectUpdater")
+local Box = require("Box")
 
 local Particle = {}
+
+
+function Particle:New(data)
+
+
+	local object = {}
+
+	object.x = data.x or 100
+	object.y = data.y or 100
+
+	object.box = Box:New
+	{
+		x = object.x,
+		y = object.y,
+		color = data.color
+	}
+
+	object.xSpeed = data.xSpeed or 0
+	object.ySpeed = data.ySpeed or 1
+	object.speed = data.speed or 10
+	object.damp = data.damp or 0
+
+	object.fade = data.fade or 10
+	object.fadeCount = 0
+
+
+	function object:Move()
+		self.x = self.x + (self.speed * self.xSpeed)
+		self.y = self.y + (self.speed * self.ySpeed)
+
+		self.box.x = self.x
+		self.box.y = self.y
+	end 
+
+
+	function object:Update()
+
+	end 
+
+
+	ObjectUpdater:Add{object}
+
+	return object
+
+
+end 
+
+
 
 
 
